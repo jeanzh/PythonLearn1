0b32bb571f94ca970091434ec720ff59107f1852
赵坚 <zhaojian@msn.com>
2017-10-30 16:35:56 +0800

    增加汉诺塔

--- PythonLearn/PythonLearn.py
+++ PythonLearn/PythonLearn.py
@@ -5,4 +5,21 @@ if a>=0:
     print(a)
 else:
     print(-a)
- 
+
+#完成汉诺塔，并打印移动步骤
+han=[[1,2,3,4],[],[]]
+
+def move(aline,bline):
+    han[bline-1].insert(0,han[aline-1][0])
+    han[aline-1].pop(0)
+
+def hanoi(aline,bline,cline,cnt):
+    if cnt==1:
+        move(aline,cline)
+        print(han )
+    else:
+        hanoi(aline,cline,bline,cnt-1)
+        hanoi(aline,bline,cline,1)
+        hanoi(bline,aline,cline,cnt-1)
+        
+hanoi(1,2,3,4)
