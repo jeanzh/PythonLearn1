93bd0e5b4306680304b6556f7b57e824d3abdc9f
赵坚 <zhaojian@msn.com>
2017-10-31 10:18:59 +0800

    更改

---
+++ PythonLearn/Fibonacci.py
@@ -0,0 +1,10 @@
+def fib(num):
+    a=0
+    b=1
+    while num >0:
+        print(b)
+        t=b
+        b=a+b
+        a=t
+        num=num-1
+fib(16)
--- PythonLearn/PythonLearn.py
+++ PythonLearn/PythonLearn.py
@@ -1,13 +1,2 @@
 
-for i,value in enumerate(["a","b","c"]):
-    print(i,value)
-def fib(num):
-    a=0
-    b=1
-    while num >0:
-        print(b)
-        t=b
-        b=a+b
-        a=t
-        num=num-1
-fib(16)
+
